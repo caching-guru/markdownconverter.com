4d205e57fdd555726c7c1d890e72eb434c4ce298
Ralph Küpper <ralph.kuepper@skelpo.com>
2023-02-15 11:50:04 +0100

    adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public mode = 1;
+  public output:any = null;
+  public input = "";
   title = 'markdownconverter';
+
+  constructor(
+    private http: HttpClient,
+    private dom: DomSanitizer
+  ) {
+
+  }
+
+  m1() {
+    this.mode = 1;
+  }
+
+  m2() {
+    this.mode = 2;
+  }
+  outputText = "";
+  copyToClipboard() {
+    let l = (e: ClipboardEvent) => {
+      e.clipboardData!.setData('text/plain', (this.outputText));
+      e.preventDefault();
+      document.removeEventListener('copy', l);
+      alert('Copied.');
+    };
+    document.addEventListener('copy', l);
+    document.execCommand('copy');
+  }
+
+
+  convert() {
+    let url = "https://api.caching.guru/v1/markdown/getHTML";
+    if (this.mode == 2) {
+      url = "https://api.caching.guru/v1/markdown/getMarkdown";
+    }
+    this.http.post<any>(url, {"input": this.input}).subscribe(r => {
+      this.output = this.dom.bypassSecurityTrustHtml(r.output);
+      this.outputText = r.output;
+    })
+  }
+
+  ngOnInit(): void {
+      
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,9 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
